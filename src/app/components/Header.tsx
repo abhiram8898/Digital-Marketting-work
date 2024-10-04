3b4c2de9c50d490a844244f2181e78c73b5d4a2e
Abhiram <57166985+abhiram8898@users.noreply.github.com>
2024-10-04 13:32:29 +0530

    Add files via upload

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Flex, ToggleButton } from "@/once-ui/components";
+import styles from "@/app/components/Header.module.scss";
+import { routes, display } from "@/app/resources";
+import { person, home, about, work } from "@/app/resources";
+import React from "react";
+
+export const Header = () => {
+  const pathname = usePathname() ?? "";
+
+  return (
+    <Flex
+      style={{ height: "fit-content" }}
+      className={styles.position}
+      as="header"
+      zIndex={9}
+      fillWidth
+      padding="8"
+      justifyContent="center"
+    >
+      <Flex
+        hide="s"
+        paddingLeft="12"
+        fillWidth
+        alignItems="center"
+        textVariant="body-default-s"
+      >
+        {display.location && <>{person.location}</>}
+      </Flex>
+      <Flex
+        background="surface"
+        border="neutral-medium"
+        borderStyle="solid-1"
+        radius="m-4"
+        shadow="l"
+        padding="4"
+        justifyContent="center"
+      >
+        <Flex gap="4" textVariant="body-default-s">
+          {routes["/"] && (
+            <ToggleButton
+              prefixIcon="home"
+              href="/"
+              selected={pathname === "/"}
+            >
+              <Flex paddingX="2" hide="s">
+                {home.label}
+              </Flex>
+            </ToggleButton>
+          )}
+          {routes["/about"] && (
+            <ToggleButton
+              prefixIcon="person"
+              href="/about"
+              selected={pathname === "/about"}
+            >
+              <Flex paddingX="2" hide="s">
+                {about.label}
+              </Flex>
+            </ToggleButton>
+          )}
+          {routes["/work"] && (
+            <ToggleButton
+              prefixIcon="grid"
+              href="/work"
+              selected={pathname.startsWith("/work")}
+            >
+              <Flex paddingX="2" hide="s">
+                {work.label}
+              </Flex>
+            </ToggleButton>
+          )}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
---
+++ src/app/resources/content-blog.js
@@ -0,0 +1,303 @@
+import { InlineCode } from "@/once-ui/components";
+import Link from "next/link";
+
+const person = {
+  firstName: "Christeena",
+  lastName: "Simon",
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  role: "Digital Marketting Manager",
+  avatar: "/images/avatar.jpg",
+  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+};
+
+const social = [
+  // Links are automatically displayed.
+  // Import new icons in /once-ui/icons.ts
+  {
+    name: "GitHub",
+    icon: "github",
+    link: "https://github.com/once-ui-system/nextjs-starter",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    link: "https://www.linkedin.com/company/once-ui/",
+  },
+  {
+    name: "X",
+    icon: "x",
+    link: "",
+  },
+  {
+    name: "Email",
+    icon: "email",
+    link: "mailto:example@gmail.com",
+  },
+];
+
+const home = {
+  label: "Home",
+  title: `${person.name}'s Portfolio`,
+  description: `Portfolio website showcasing my work as a ${person.role}`,
+  headline: <>Digital Marketting Manger</>,
+  subline: (
+    <>
+      I'm Christeena, a design engineer at <InlineCode>FLY</InlineCode>, where I
+      craft intuitive user experiences. After hours, I build my own projects.
+    </>
+  ),
+};
+
+const about = {
+  label: "About",
+  title: "About me",
+  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  tableOfContent: {
+    display: false,
+    subItems: false,
+  },
+  avatar: {
+    display: false,
+  },
+  calendar: {
+    display: false,
+    link: "https://cal.com",
+  },
+  intro: {
+    display: true,
+    title: "Introduction",
+    description: (
+      <>
+        <p>
+          Selene is a Jakarta-based design engineer with a passion for
+          transforming complex challenges into simple, elegant design solutions.
+          Her work spans digital interfaces, interactive experiences, and the
+          convergence of design and technology.
+        </p>
+        <p>
+          My work spans a diverse range of disciplines, from crafting{" "}
+          <Link href="/work">intuitive digital interfaces</Link> to designing
+          immersive interactive experiences. I’m particularly interested in the
+          intersection of design and engineering, where aesthetics meet
+          functionality. I believe that the best solutions arise from a balance
+          of creativity and technical rigor, and I enjoy the challenge of
+          finding that balance in every project I undertake.
+        </p>
+      </>
+    ),
+  },
+  work: {
+    display: false, // set to false to hide this section
+    title: "Work Experience",
+    experiences: [
+      {
+        company: "Brackenberry",
+        timeframe: "2024 - Present",
+        role: "Digital Marketing Consultant",
+        Work: [
+          <>
+            Overall management of Digital Media Marketing. End-to-end analysis,
+            delivery and performance reporting of all digital elements including
+            creatively led digital campaigns.
+          </>,
+          <> Creation of innovative strategies and posts.</>,
+        ],
+        images: [
+          // optional: leave the array empty if you don't want to display images
+          {
+            src: "/images/projects/project-01/cover-01.jpg",
+            alt: "Once UI Project",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        company: "Top Level Builders and Developers",
+        timeframe: "2023 - 2024",
+        role: "Digital Marketing Manager",
+        Work: [
+          <>
+            Measure and report the performance of all digital marketing
+            campaigns and assess against goals (ROI and KPIs).
+          </>,
+          <>
+            Develop and implement various strategies by providing creative
+            solutions to improve businesses.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: "Bictree",
+        timeframe: "2022 - 2023",
+        role: "Digital Marketing Executive and Content Creator",
+        Work: [
+          <>
+            – Manage websites and social media, as well as SEO content for
+            branding purposes.
+          </>,
+          <>
+            Prepare posts about the latest industry-related topics, produce
+            marketing copy for advertising products, and other content for
+            promotion and business.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: "Homeskul Innolearn Pvt.Ltd",
+        timeframe: "2021 - 2022",
+        role: "Content Editor",
+        Work: [
+          <>Created content for the company’s social media platforms.</>,
+          <>
+            Worked collaboratively with the operations team in the preparation
+            of sophisticated internal documents.
+          </>,
+        ],
+        images: [],
+      },
+    ],
+  },
+  studies: {
+    display: false, // set to false to hide this section
+    title: "Studies",
+    institutions: [
+      {
+        name: "University of Jakarta",
+        description: <>Studied software engineering.</>,
+      },
+      {
+        name: "Build the Future",
+        description: <>Studied online marketing and personal branding.</>,
+      },
+    ],
+  },
+  technical: {
+    display: false, // set to false to hide this section
+    title: "Technical skills",
+    skills: [
+      {
+        title: "Figma",
+        description: (
+          <>Able to prototype in Figma with Once UI with unnatural speed.</>
+        ),
+        images: [
+          {
+            src: "/images/projects/project-01/cover-02.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+          {
+            src: "/images/projects/project-01/cover-03.jpg",
+            alt: "Project image",
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const blog = {
+  label: "Blog",
+  title: "Writing about design and tech...",
+  description: `Read what ${person.name} has been up to recently`,
+  // Create new blog posts by adding a new .mdx file to app/blog/posts
+  // All posts will be listed on the /blog route
+};
+
+const work = {
+  label: "Work",
+  title: "My projects",
+  description: `Design and dev projects by ${person.name}`,
+  // Create new project pages by adding a new .mdx file to app/blog/posts
+  // All projects will be listed on the /home and /work routes
+};
+
+const gallery = {
+  label: "Gallery",
+  title: "My photo gallery",
+  description: `A photo collection by ${person.name}`,
+  // Images from https://pexels.com
+  images: [
+    {
+      src: "/images/gallery/img-01.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-02.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-03.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-04.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-05.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-06.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-07.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-08.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-09.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-10.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-11.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-12.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-13.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-14.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+  ],
+};
+
+export { person, social, newsletter, home, about, blog, work, gallery };
