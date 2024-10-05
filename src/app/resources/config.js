24e9cd4e58c1cb0af819ca81fad311bedc09a67f
Abhiram <57166985+abhiram8898@users.noreply.github.com>
2024-10-05 11:46:57 +0530

    Update config.js

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -21,7 +21,7 @@ const effects = {
 }
 
 const style = {
-    theme:       'dark',         // dark | light
+    theme:       'light',         // dark | light
     neutral:     'gray',         // sand | gray | slate
     brand:       'aqua',         // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     accent:      'yellow',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
